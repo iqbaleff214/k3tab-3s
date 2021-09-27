7f23ea8b7cbe9ab1974ba0d4b3ab5876203f190e
M. Iqbal Effendi <iqbaleff214@gmail.com>
2021-09-27 10:02:59 +0800

    Fix serviceman dashboard

--- resources/views/pages/mechanic/dashboard.blade.php
+++ resources/views/pages/mechanic/dashboard.blade.php
@@ -10,47 +10,38 @@
                         <h1>{{ $title }}</h1>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12">
-                        <form action="{{ url()->current() }}" method="GET">
-                            <div class="input-group mt-2">
-                                <input type="search" name="q" class="form-control form-control-lg"
-                                       placeholder="Find the tool by it name or equipment number"
-                                       value="{{ $_GET['q'] ?? '' }}" autofocus>
-                                <div class="input-group-append">
-                                    <button type="submit" class="btn btn-lg btn-dark">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            </div><!-- /.container-fluid -->
         </section>
 
         <!-- Main content -->
-        <div class="content">
-            <div class="container mt-3">
-                <div class="card-columns">
-                </div>
+        <section class="content">
 
-                <!-- Data Loader -->
-                <div class="auto-load text-center py-5">
-                    <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg"
-                         xmlns:xlink="http://www.w3.org/1999/xlink"
-                         x="0px" y="0px" height="60" viewBox="0 0 100 100" enable-background="new 0 0 0 0"
-                         xml:space="preserve">
-                            <path fill="#000"
-                                  d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                                <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s"
-                                                  from="0 50 50" to="360 50 50" repeatCount="indefinite"/>
-                            </path>
-                        </svg>
+            <!-- Default box -->
+            <div class="card card-yellow card-outline">
+                <div class="card-header">
+                    <h3 class="card-title">Title</h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
                 </div>
-                <!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
+                <div class="card-body">
+                    Start creating your amazing application!
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                    Footer
+                </div>
+                <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+
+        </section>
         <!-- /.content -->
     </div>
 @endsection
@@ -58,55 +49,9 @@
 @push('script')
     <script>
         window.addEventListener('load', function () {
-            var ENDPOINT = "{{ route('mechanic.tool.data') }}";
-            var SEARCH = "{{ $_GET['q'] ?? '' }}";
-            var page = 1;
-
-            $(document).ready(function () {
-                loadMore(page);
-                window.addEventListener('scroll', () => {
-                    const {scrollHeight, scrollTop, clientHeight} = document.documentElement;
-                    if (scrollTop + clientHeight >= scrollHeight) {
-                        page++;
-                        loadMore(page);
-                    }
-                });
+            $(function () {
+                console.log('what');
             });
-
-            function loadMore(page) {
-                let URL = ENDPOINT + "?page=" + page;
-                if (SEARCH !== "") URL += "&q=" + SEARCH;
-                $.ajax({
-                    url: URL,
-                    datatype: "html",
-                    type: "get",
-                    beforeSend: function () {
-                        $('.auto-load').show();
-                    }
-                }).done(function (response) {
-                    let card = ``;
-                    response.data.forEach(e => {
-                        card += `
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="font-weight-bold">` + e.description + `</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">Eq: ` + e.equipment_number + `</h6>
-                                    <p class="card-text">` + e.additional_description + `</p>
-                                    <a href="#" class="card-link">Card link</a>
-                                    <a href="#" class="card-link">Another link</a>
-                                </div>
-                            </div>`;
-                    });
-                    if (response.data.length == 0) {
-                        $('.auto-load').html("We don't have more data to display.");
-                        return;
-                    }
-                    $('.auto-load').hide();
-                    $(".card-columns").append(card);
-                }).fail(function (jqXHR, ajaxOptions, thrownError) {
-                    console.log('Server error occured');
-                });
-            }
         });
     </script>
 @endpush
